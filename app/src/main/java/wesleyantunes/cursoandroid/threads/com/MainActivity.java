3d077bde72ca1419fabc3b5ca2fc1d112846f115
Wesley.antunesdc <wesleyantunesdca@gmail.com>
2021-03-05 11:54:12 -0300

    Criação de Threads

--- app/src/main/java/wesleyantunes/cursoandroid/threads/com/MainActivity.java
+++ app/src/main/java/wesleyantunes/cursoandroid/threads/com/MainActivity.java
@@ -3,7 +3,9 @@ package wesleyantunes.cursoandroid.threads.com;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +16,39 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void iniciarThread(View view){
-
+       /* MyThread myThread = new MyThread();
+        Toast.makeText(getApplicationContext(),"Executando Thread em segundo plano.",Toast.LENGTH_SHORT).show();
+        myThread.start();*/
+        MyRunnable runnable = new MyRunnable();
+        new Thread(runnable).start();
     }
+
+    class MyThread extends Thread{
+        @Override
+        public void run() {
+            for(int i=0; i<=15; i++){
+                Log.d("Thread", "Contador: "+i);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    class MyRunnable implements Runnable{
+        @Override
+        public void run() {
+            for(int i=0; i<=15; i++){
+                Log.d("Thread", "Contador: "+i);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
 }
